b1e2e019226987000578c1e5accc8c17127b0e62
Bachuk, Alex <alex@bachuk.com>
2013-10-01 17:11:24 -0400

    read me, db driver

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-expressjs_mongo
+Express.js 3 with MongoDB boilerplate
 ===============
+1. install the latest version of MongoDB from http://mongodb.org/downloads
+2. cd into the folder and do `npm install`
+3. start mongo server with `mongod`, you should see stuff in localhost:28017
+4. 
--- app.js
+++ app.js
@@ -9,7 +9,39 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var util = require('util');
+var querystring = require('querystring');
+var mongodb = require ('mongodb');  
 var app = express();
+
+
+var Db = mongodb.Db;
+var Connection = mongodb.Connection; 
+var Server = mongodb.Server;
+var host = '127.0.0.1';
+var port = 27017;
+var db=new Db ('users_db', new Server(host,port, {}));
+
+db.open(function(e, c) {
+// console.log (util.inspect(db));
+// creates server
+var server = http.createServer(function(req, res) {
+    //sets the right header and status code
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+});
+    //outputs string with line end symbol
+    res.end(db._state);
+  });
+  //sets port and IP address of the server
+server.listen(port, function() { console.log(
+      'Server is running at %s:%s ',
+    server.address().address,
+    server.address().port);
+});
+  db.close();
+});
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
